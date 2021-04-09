e371751d422338c814082081a92be503e61c738a
Martin Winks <cat@snejugal.ru>
2021-04-09 14:33:16 +0300

    hotfix: remove migration application

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,6 @@ set -o nounset
 
 readonly cmd="$*"
 
-for script in ./sql/up_*.sql; do
-  echo "applying ${script}"
-  # {executable} {db_dsn} {sql_script} {retry_times} {timeout}
-  ./sql/up-db.py "${DB_DSN}" "${script}" 5 3
-done
-
 
 # shellcheck disable=SC2086
 exec $cmd
