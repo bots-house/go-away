cf8398926d0a8443eefcb9de79bf7766f61215cd
Martin Winks <cat@snejugal.ru>
2021-04-09 14:39:17 +0300

    hotfix: add contstraints

--- src/go_away/core/init_db.py
+++ src/go_away/core/init_db.py
@@ -11,6 +11,6 @@ async def make_tables(
     config: Config,
     table_clss: Tuple[Type[BaseDataTable], ...],
 ) -> Tuple[BaseDataTable, ...]:
-    shared_pool = await create_pool(config.db_dsn)
+    shared_pool = await create_pool(config.db_dsn, min_size=3, max_size=3)
 
     return tuple(table_cls(shared_pool) for table_cls in table_clss)
